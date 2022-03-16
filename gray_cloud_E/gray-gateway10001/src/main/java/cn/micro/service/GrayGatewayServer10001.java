f3b9f152a79616c287d00f5508758b80fb1c04ba
dongxie <dongxie@sohu-inc.com>
2022-03-16 16:27:15 +0800

    gray

---
+++ gray_cloud_E/gray-gateway10001/src/main/java/cn/micro/service/GrayGatewayServer10001.java
@@ -0,0 +1,14 @@
+package cn.micro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dongxie on 2022/3/16.
+ */
+@SpringBootApplication
+public class GrayGatewayServer10001 {
+    public static void main(String[] args) {
+        SpringApplication.run(GrayGatewayServer10001.class, args);
+    }
+}
